5cabfefee55e22e8c44ba3e549363e855e2073ad
Angela-coello <angeladarko32@gmail.com>
2023-06-27 15:24:23 +0200

    card finish

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
+import CardPoke from '../components/CardPoke.vue';
+const numberOfCards = 20;
 </script>
 
 <template>
   <main>
     <!-- card poke, bucle for 1 tarjeta por cada pokemon-->
-    <h1>Esto es home view</h1>
+    <div class="pipi">
+      <CardPoke v-for="index in numberOfCards" :key="index" />
+    </div>
+   
   </main>
 </template>
+
+<style scoped>
+
+main {
+  background: #efefbb;
+  background: linear-gradient(to right, #d4d3dd, #efefbb);
+  font-family: "Lato", sans-serif;
+  display: flex;
+ 
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+}
+ .pipi {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+ }
+</style>
+
+
