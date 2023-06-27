33a9e177de99082f753c7195db9f7a40d47f3c10
GARS00 <guillermoandres.rs@gmail.com>
2023-06-27 15:42:33 +0200

    Starting Header

--- src/components/PokeHeader.vue
+++ src/components/PokeHeader.vue
@@ -5,5 +5,30 @@ fetchData();
 </script>
 
 <template>
-    <h1>Hola esto es un h1 de componente</h1>
+    <header>
+        <div id="RightSide">
+            <h1>POKEDEX</h1>
+            <img src="src\assets\pokeball 1.png" alt="Pokeball">
+        </div>
+        <div id="SearchBar">
+
+        </div>
+    </header>
 </template>
+
+<style scoped>
+    header {
+        width: 100vw;
+        height: 12vh;
+        background-color: blue;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif ;
+        padding: 1%;
+    }
+    #RightSide {
+        display: flex;
+        align-items: center;
+        gap: 1%;
+        height: 10%;
+    }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
+
 </script>
 
 <template>
   <main>
-    <h1>Esto es home view</h1>
-    <!-- card poke, bucle for 1 tarjeta por cada pokemon-->
   </main>
 </template>
